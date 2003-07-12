4764d3690558c39be52c06d1c56ffdebea103bb5
bbergstrand <>
2003-07-12 04:41:20 +0000

    Backed out max file size enforcement; it isn't needed now that we support large files.

--- src/gnu/ext2fs/ext2_readwrite.c
+++ src/gnu/ext2fs/ext2_readwrite.c
@@ -118,10 +118,10 @@ READ(ap)
 		panic("%s: type %d", READ_S, vp->v_type);
 #endif
 	fs = ip->I_FS;
-
+#if 0
 	if ((u_quad_t)uio->uio_offset > fs->s_maxfilesize)
-
 		ext2_trace_return (EFBIG);
+#endif
 
 	orig_resid = uio->uio_resid;
    #ifdef APPLE
@@ -260,11 +260,11 @@ WRITE(ap)
 	}
 
 	fs = ip->I_FS;
-
+#if 0
 	if (uio->uio_offset < 0 ||
 	    (u_quad_t)uio->uio_offset + uio->uio_resid > fs->s_maxfilesize)
 		ext2_trace_return(EFBIG);
-
+#endif
 	/*
 	 * Maybe this should be above the vnode op call, but so long as
 	 * file servers have no limits, I don't think it matters.
