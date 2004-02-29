dd1556e4deb0c07ffc9e35b0fbba0e0fc5dced9f
bbergstrand <>
2004-02-29 09:35:15 +0000

    Added code to copy missing kernel headers

--- setup.sh
+++ setup.sh
@@ -3,10 +3,46 @@
 
 PATH=/usr/bin:/bin
 
+SYSVER=`uname -r | awk -F. '{print $1}'`
+
 if [ ! -d ./src/gnu ]; then
 	echo "Invalid source directory."
 	exit 1
 fi
+
+#args = src,dest
+copydir() {
+	for i in ${1}
+	do
+		#echo ${2}/`basename $i`
+		if [ ! -d $i ]; then
+			if [ ! -e ${2}/`basename $i` ]; then
+				echo sudo cp $i ${2}/
+			fi
+		else
+			if [ ! -d ${2}/`basename $i` ]; then
+				echo sudo cp -R $i ${2}/
+			else 
+				copydir "`ls -d $i/* | grep -v CVS`" ${2}/`basename $i`
+			fi
+		fi
+		
+	done
+}
+
+# copy missing kernel headers
+SYS_KERNF=/System/Library/Frameworks/Kernel.framework
+KERNH=./src/depend/panther/kern/Headers
+if [ ${SYSVER} -eq 6 ]; then
+KERNH=./src/depend/jaguar/kern/Headers
+fi
+
+if [ ! -d ${SYS_KERNF} ]; then
+echo "Warning: Kernel framework is not installed. Bailing."
+exit 1
+fi
+
+copydir "`ls -d ${KERNH}/* | grep -v CVS`" ${SYS_KERNF}/Headers 
 
 #perms
 
