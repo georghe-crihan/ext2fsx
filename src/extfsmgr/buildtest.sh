dcd547d4db37e9d61f7d6c0116c51159e7d01a96
bbergstrand <>
2004-03-29 20:25:40 +0000

    Fixed problems prevent compile.

--- src/extfsmgr/buildtest.sh
+++ src/extfsmgr/buildtest.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cc -F/System/Library/PrivateFrameworks \
 -framework Foundation -framework IOKit -framework DiskArbitration \
--g -I./ -Icore/ -I../ -o extmgr test_main.m core/ExtFSMediaController.m core/ExtFSMedia.m
+-g -I. -Icoremgr -I.. -I../gnu/ext2fs/linux/include -o extmgr \
+test_main.m coremgr/ExtFSMediaController.m coremgr/ExtFSMedia.m
