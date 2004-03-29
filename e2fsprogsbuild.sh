7da2df7234d48bcf5ce2fff6563abd5893261ad0
bbergstrand <>
2004-03-29 20:07:09 +0000

    Changed SRCROOT var to EXT2BUILD for compat. with MakeInstall.sh. Added copyright.

--- e2fsprogsbuild.sh
+++ e2fsprogsbuild.sh
@@ -1,19 +1,46 @@
 #!/bin/sh
 # $Id$
+#
+# Builds e2fsprogs
+#
+# Created for the ext2fsx project: http://sourceforge.net/projects/ext2fsx/
+#
+# Copyright 2003-2004 Brian Bergstrand.
+#
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+#
+# 1.    Redistributions of source code must retain the above copyright notice, this list of
+#     conditions and the following disclaimer.
+# 2.    Redistributions in binary form must reproduce the above copyright notice, this list of
+#     conditions and the following disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+# 3.    The name of the author may not be used to endorse or promote products derived from this
+#     software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 
 #src root
-if [ -z "${SRCROOT}" ]; then
-	echo "SRCROOT is not defined, using current directory"
-	SRCROOT="."
+if [ -z "${EXT2BUILD}" ]; then
+	echo "EXT2BUILD is not defined, using current directory"
+	EXT2BUILD="."
 fi
 
-if [ ! -d "${SRCROOT}/src/e2fsprogs" ]; then
+if [ ! -d "${EXT2BUILD}/src/e2fsprogs" ]; then
 	exit 1
 fi
 
-cd "${SRCROOT}/src/e2fsprogs"
+cd "${EXT2BUILD}/src/e2fsprogs"
 
 E2VER=`sed -n -e "s/^.*E2FSPROGS_VERSION[^0-9]*\([0-9]*\.[0-9]*\).*$/\1/p" ./version.h`
 
